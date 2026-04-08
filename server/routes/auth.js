621eb96c14646c51e5c529435e4afc05b758176c
yakovlevpavel21 <pashok20032012@mail.ru>
2026-04-08 12:51:50 +0300

    фикс

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const prisma = require('../db');
+const { protect } = require('../auth');
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
